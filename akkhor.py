556d960a31191d6eca0eac99f9b62fd8a91462c1
Karar rifat <kararrifat@gmail.com>
2022-05-11 16:55:32 +0600

    2nd

--- akkhor.py
+++ akkhor.py
@@ -5,8 +5,6 @@ workbook = xl.Workbook()
 sheet = workbook.active
 workbook.save("akkhor.xlsx")
 
-# file = open('2.txt', 'r')
-# f = file.readlines()
 question_flag = False
 ko_option_flag = False
 kho_option_flag = False
@@ -16,61 +14,137 @@ answer_flag = False
 definition_flag = False
 question = ""
 option = ""
+answer = ""
+definition = ""
+
+row = 0
 
 with open('2.txt', 'r') as file:
     for line in file:
         # print(line)
         if line == "৩.১ এবং ৩.২ অর্থ সমমূল্যের ধারণা ও গুরুত্ব:\n":
             break
+        # if regex.search("([অ-য়]+ অধ্যায়)|([০-৯৯][অ-য়] অধ্যায়)", line):
+        #     break
+        # if row == 4:
+        #     break
         for word in line.split():
 
+            #  [q o1 o2 o3 o4 ans def]
             # question
             if regex.search("[০-৯৯]।", word):
                 # print(word)
                 # raise a question flag
-                gho_option_flag = False
-                print(option)
-                option = ""
+                if answer_flag:
+                    answer_flag = False
+                    print(answer)
+                    sheet.cell(row, 6, answer)
+                    answer = ""
+                if definition_flag:
+                    definition_flag = False
+                    print(definition)
+                    sheet.cell(row, 7, definition)
+                    definition = ""
+                if ko_option_flag:
+                    ko_option_flag = False
+                    print(option)
+                    sheet.cell(row, 2, option)
+                    option = ""
+                if kho_option_flag:
+                    kho_option_flag = False
+                    print(option)
+                    sheet.cell(row, 3, option)
+                    option = ""
+                if go_option_flag:
+                    go_option_flag = False
+                    print(option)
+                    sheet.cell(row, 4, option)
+                    option = ""
+                if gho_option_flag:
+                    gho_option_flag = False
+                    print(option)
+                    sheet.cell(row, 5, option)
+                    option = ""
+                row += 1
                 question_flag = True
                 # continue
-
+            #
             # options
             ko = ""
             kho = ""
             go = ""
             gho = ""
 
-            if word == "(ক)":
-                question_flag = False
-                print(question)
-                question = ""
-                ko_option_flag = True
-                # continue
+            if not answer_flag:
+                if word == "(ক)":
+                    question_flag = False
+                    print(question)
+                    sheet.cell(row, 1, question)
+                    question = ""
+                    ko_option_flag = True
+                    # continue
 
-                # print("ko")
+                    # print("ko")
 
-            if word == "(খ)":
-                # print("kho")
-                ko_option_flag = False
-                print(option)
-                option = ""
-                kho_option_flag = True
-                # continue
+                if word == "(খ)":
+                    # print("kho")
+                    ko_option_flag = False
+                    print(option)
+                    sheet.cell(row, 2, option)
+                    option = ""
+                    kho_option_flag = True
+                    # continue
 
-            if word == "(গ)":
-                kho_option_flag = False
-                print(option)
-                option = ""
-                go_option_flag = True
-                # continue
+                if word == "(গ)":
+                    kho_option_flag = False
+                    print(option)
+                    sheet.cell(row, 3, option)
+                    option = ""
+                    go_option_flag = True
+                    # continue
 
-            if word == "(ঘ)":
-                go_option_flag = False
-                print(option)
-                option = ""
-                gho_option_flag = True
-                # continue
+                if word == "(ঘ)":
+                    go_option_flag = False
+                    print(option)
+                    sheet.cell(row, 4, option)
+                    option = ""
+                    gho_option_flag = True
+                    # continue
 
+            # answer
+            if word == "উত্তরঃ":
+                if gho_option_flag:
+                    gho_option_flag = False
+                    print(option)
+                    sheet.cell(row, 5, option)
+                    option = ""
+                if definition_flag:
+                    definition_flag = False
+                    print(definition)
+                    sheet.cell(row, 7, definition)
+                    definition = ""
+                if question_flag:
+                    question_flag = False
+                    print(question)
+                    sheet.cell(row, 1, question)
+                    question = ""
+                answer_flag = True
+
+            # definition
+            if word == "ব্যাখ্যাঃ" or word == "ব্যাখাঃ":
+                if gho_option_flag:
+                    gho_option_flag = False
+                    print(option)
+                    sheet.cell(row, 5, option)
+                    option = ""
+                if answer_flag:
+                    answer_flag = False
+                    print(answer)
+                    sheet.cell(row, 6, answer)
+                    answer = ""
+                definition_flag = True
+
+            # add to doc file
             if question_flag:
                 question += " " + word
             if ko_option_flag:
@@ -81,28 +155,11 @@ with open('2.txt', 'r') as file:
                 option += " " + word
             if gho_option_flag:
                 option += " " + word
+            if answer_flag:
+                answer += " " + word
+            if definition_flag:
+                definition += " " + word
 
-# print(f)
-# i = 0
-# row = 1
-# column = 1
-# while i != 11:
-#     # add question
-#     x = ""
-#     if regex.search("[০-৫০০]।", f[i]):
-#         x = regex.sub("[০-৫০০]।", "", f[i])
-#         i = i + 1
-#     if()
-#     while f[i].find("(") == -1:
-#         print(f[i])
-#         x += f[i]
-#         i = i + 1
-#     sheet.cell(row, column, x)
-#     # if regex.search("[()]", f[i]):
-#
-#     # if regex.search()
-#     row += 1
-#     i += 1
-#     workbook.save('akkhor.xlsx')
-#     if i >= len(f):
-#         break
+workbook.save("akkhor.xlsx")
+
+
